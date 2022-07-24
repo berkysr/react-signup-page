c2232d7054eaa0265dbbd2a7520a6f99b3d7a31d
berkysr <brk.yasar@gmail.com>
2022-07-25 00:54:30 +0300

    feature: signup page add

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import logo from './logo.svg';
 import './App.scss';
 import ButtonComponent from './components/button.component';
 import NavigationComponent from './components/navigation.component';
+import FooterComponent from './components/footer.component';
+import SignUpPage from './pages/sign-up.page';
 
 function App() {
   return (
     <div className="App">
       <NavigationComponent></NavigationComponent>
+      <SignUpPage></SignUpPage>
+      <FooterComponent></FooterComponent>
     </div>
   );
 }
---
+++ src/components/footer.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+export default function NavbarComponent() {
+  return (
+    <Box>
+      <div className="sticky bot-0">Contact Us</div>
+    </Box>
+  );
+}
