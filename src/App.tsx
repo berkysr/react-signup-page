caa9818b613892edb0d404d22a083899c8440acc
berkysr <brk.yasar@gmail.com>
2022-07-25 00:45:17 +0300

    feature: create ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import ButtonComponent from './components/Button';
+import ButtonComponent from './components/button.component';
+import NavigationComponent from './components/navigation.component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-6xl'>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <ButtonComponent></ButtonComponent>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationComponent></NavigationComponent>
     </div>
   );
 }
